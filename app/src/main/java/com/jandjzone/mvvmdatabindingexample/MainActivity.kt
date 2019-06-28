7bdb70135bf770e0a660dcb82106a9670c63b8e4
John <john.kuang@liveblockauctions.com>
2019-06-28 14:09:05 -0600

    cleaning unused imports

--- app/src/main/java/com/jandjzone/mvvmdatabindingexample/MainActivity.kt
+++ app/src/main/java/com/jandjzone/mvvmdatabindingexample/MainActivity.kt
@@ -4,8 +4,6 @@ import android.content.Context
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
